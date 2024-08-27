f5e1cf117a185082eb44315a0b9af43692555057
Nadia Islam <sohasara853@gmail.com>
2024-08-27 20:22:01 +0600

    add api keys

---
+++ lib/constants/api_keys.dart
@@ -0,0 +1,19 @@
+class ApiKeys {
+  final String nature =
+      'https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=multiple';
+  final String gk =
+      'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple';
+  final String flim =
+      'https://opentdb.com/api.php?amount=10&category=11&difficulty=hard&type=multiple';
+  final String math =
+      'https://opentdb.com/api.php?amount=10&category=19&difficulty=medium';
+  final String computer =
+      'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium';
+  final String spots =
+      'https://opentdb.com/api.php?amount=10&category=21&difficulty=medium';
+
+  final String geo =
+      'https://opentdb.com/api.php?amount=10&category=22&difficulty=medium';
+  final String politics =
+      'https://opentdb.com/api.php?amount=10&category=24&difficulty=medium&type=multiple';
+}
