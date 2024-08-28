33faebc538517e9bb7778138c9eb1cc4d8473a8b
Nadia Islam <sohasara853@gmail.com>
2024-08-28 22:41:49 +0600

    modify the data

--- lib/data/container_data.dart
+++ lib/data/container_data.dart
@@ -4,9 +4,29 @@ final dataImage = [
     'imageurl': 'assets/football.png',
     'text': 'Sports',
   },
-  {},
-  {},
-  {},
-  {},
-  {},
+  {
+    'index': '2',
+    'imageurl': 'assets/math.png',
+    'text': 'Math',
+  },
+  {
+    'index': '3',
+    'imageurl': 'assets/film.png',
+    'text': 'Flims',
+  },
+  {
+    'index': '4',
+    'imageurl': 'assets/geo.png',
+    'text': 'Geography',
+  },
+  {
+    'index': '5',
+    'imageurl': 'assets/gk.png',
+    'text': 'GK',
+  },
+  {
+    'index': '6',
+    'imageurl': 'assets/politic.png',
+    'text': 'Politic',
+  },
 ];
