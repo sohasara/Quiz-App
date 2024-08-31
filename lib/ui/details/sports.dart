dfba97949fe738da00bc7107fefe434d9b1f7043
Nadia Islam <sohasara853@gmail.com>
2024-08-31 19:54:26 +0600

    add gradient

--- lib/ui/details/sports.dart
+++ lib/ui/details/sports.dart
@@ -6,7 +6,24 @@ class SportsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.purple[300]!,
+              Colors.purple[100]!,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(),
+      ),
     );
   }
 }
