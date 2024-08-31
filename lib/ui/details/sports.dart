48b918047ad4712ed4c04fd404fbe1223a01fefa
Nadia Islam <sohasara853@gmail.com>
2024-08-31 15:44:06 +0600

    add appbar

---
+++ lib/ui/details/sports.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SportsPage extends StatelessWidget {
+  const SportsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
