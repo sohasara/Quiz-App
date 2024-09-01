7379226158af8e52655b615bd6a9f940d04ee909
Nadia Islam <sohasara853@gmail.com>
2024-09-01 19:54:50 +0600

    add factory class

--- lib/data/sport_data.dart
+++ lib/data/sport_data.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 // ignore_for_file: non_constant_identifier_names
 
 class SportsData {
@@ -14,4 +16,14 @@ class SportsData {
     required this.correct_answer,
     required this.incorrect_answers,
   });
+
+  factory SportsData.fromMap(Map<String, dynamic> map) {
+    return SportsData(
+      type: map['type'] as String,
+      category: map['category'] as String,
+      question: map['question'] as String,
+      correct_answer: map['correct_answer'] as String,
+      incorrect_answers: map['incorrect_answers'] as String,
+    );
+  }
 }
