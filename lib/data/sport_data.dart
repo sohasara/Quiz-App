59740da9d37c80416f3b2915c7d5fea0a960c3b1
Nadia Islam <sohasara853@gmail.com>
2024-09-01 19:52:51 +0600

    add data class

---
+++ lib/data/sport_data.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: non_constant_identifier_names
+
+class SportsData {
+  final String type;
+  final String category;
+  final String question;
+  final String correct_answer;
+  final String incorrect_answers;
+
+  SportsData({
+    required this.type,
+    required this.category,
+    required this.question,
+    required this.correct_answer,
+    required this.incorrect_answers,
+  });
+}
