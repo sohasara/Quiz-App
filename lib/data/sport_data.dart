89ca9973f780062f1a595878d2e1fc767b65979f
Nadia Islam <sohasara853@gmail.com>
2024-09-01 20:39:08 +0600

    modify

--- lib/data/sport_data.dart
+++ lib/data/sport_data.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
 // ignore_for_file: non_constant_identifier_names
 
 class SportsData {
@@ -7,7 +5,7 @@ class SportsData {
   final String category;
   final String question;
   final String correct_answer;
-  final String incorrect_answers;
+  final List<String> incorrect_answers;
 
   SportsData({
     required this.type,
@@ -19,11 +17,12 @@ class SportsData {
 
   factory SportsData.fromMap(Map<String, dynamic> map) {
     return SportsData(
-      type: map['type'] as String,
-      category: map['category'] as String,
-      question: map['question'] as String,
-      correct_answer: map['correct_answer'] as String,
-      incorrect_answers: map['incorrect_answers'] as String,
+      type: map['type'] as String? ?? 'Unknown', // Provide default value
+      category: map['category'] as String? ?? 'General',
+      question: map['question'] as String? ?? 'No question available',
+      correct_answer: map['correct_answer'] as String? ?? 'No answer available',
+      incorrect_answers: List<String>.from(
+          map['incorrect_answers'] ?? []), // Handle possible null value
     );
   }
 }
