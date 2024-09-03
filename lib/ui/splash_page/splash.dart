5e84d81e40dde5e03c7557f6a39b09b123fe094d
Nadia Islam <sohasara853@gmail.com>
2024-09-03 13:32:49 +0600

    add icon

--- lib/ui/splash_page/splash.dart
+++ lib/ui/splash_page/splash.dart
@@ -8,60 +8,76 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purple[50],
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 220,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'QUIZ',
-                style: TextStyle(
-                  fontSize: 80,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.purple[600],
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              'assets/brain.png',
+              height: 150,
+              width: 150,
+              color: Colors.purple,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  width: 50,
                 ),
-              ),
-              Image.asset(
-                'assets/auto.png',
-                width: 90,
-                fit: BoxFit.fitWidth,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 190,
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const HomePage(),
-                ),
-              );
-            },
-            child: Container(
-              height: 65,
-              width: 330,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.purple,
-              ),
-              child: const Center(
-                child: Text(
-                  'Get Startd',
+                Text(
+                  'QUIZ',
                   style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+                    fontSize: 100,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.purple[600],
+                  ),
+                ),
+                Image.asset(
+                  'assets/auto.png',
+                  width: 90,
+                  fit: BoxFit.fitWidth,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 180,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              },
+              child: Container(
+                height: 65,
+                width: 330,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.purple,
+                ),
+                child: const Center(
+                  child: Text(
+                    'Get Startd',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
