da0308e0224726f459dce738d1214e868c0efdc2
Nadia Islam <sohasara853@gmail.com>
2024-09-15 22:17:54 +0600

    add factory class

--- lib/data/quiz_response.dart
+++ lib/data/quiz_response.dart
@@ -4,4 +4,14 @@ class QuizResponse {
   final List<QuizQuestion> results;
 
   QuizResponse({required this.results});
+
+  factory QuizResponse.fromJson(Map<String, dynamic> json) {
+    return QuizResponse(
+      results: List<QuizQuestion>.from(
+        (json['results'] as List<dynamic>).map(
+          (item) => QuizQuestion.fromJson(item as Map<String, dynamic>),
+        ),
+      ),
+    );
+  }
 }
