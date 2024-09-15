e00f33c4b95eec263d73afd1836ad096a1c96e82
Nadia Islam <sohasara853@gmail.com>
2024-09-15 22:09:14 +0600

    modify data model

--- lib/data/quiz_model.dart
+++ lib/data/quiz_model.dart
@@ -1,21 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:hive_flutter/hive_flutter.dart';
 
-@HiveType(typeId: 0)
-class QuizQuestion extends HiveObject {
-  @HiveField(0)
-  String category;
-  @HiveField(1)
-  String type;
-  @HiveField(2)
+class QuizQuestion {
   String question;
-  @HiveField(3)
+
   String correctAnswer;
-  @HiveField(4)
+
   List<String> incorrectAnswer;
   QuizQuestion({
-    required this.category,
-    required this.type,
     required this.question,
     required this.correctAnswer,
     required this.incorrectAnswer,
@@ -23,8 +14,6 @@ class QuizQuestion extends HiveObject {
 
   factory QuizQuestion.fromJson(Map<String, dynamic> json) {
     return QuizQuestion(
-      category: json['category'],
-      type: json['type'],
       question: json['question'],
       correctAnswer: json['correct_answer'],
       incorrectAnswer: List<String>.from(json['incorrect_answer']),
