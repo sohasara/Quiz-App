60138b0addabca5538d55b68e9ce69970e1bec26
Nadia Islam <sohasara853@gmail.com>
2024-09-15 22:07:57 +0600

    remove data model

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'package:quiz_app/data/quiz_model.dart';
-
 import 'package:quiz_app/ui/splash_page/splash.dart';
 
 void main() async {
   await Hive.initFlutter();
-  Hive.registerAdapter(QuizQuestionAdapter());
+
   runApp(
     const ProviderScope(
       child: MyApp(),
