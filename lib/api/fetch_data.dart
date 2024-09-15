36bfbce490bffd56a8ae7369e4df0b4d6b8c3964
Nadia Islam <sohasara853@gmail.com>
2024-09-15 19:37:46 +0600

    modify

--- lib/api/fetch_data.dart
+++ lib/api/fetch_data.dart
@@ -1,1 +1,32 @@
-class SportApi {}
+// ignore_for_file: public_member_api_docs, sort_constructors_first, non_constant_identifier_names
+class SportApi {
+  final String question;
+  final String correct_answer;
+  final List<String> incorrect_answer;
+  SportApi({
+    required this.question,
+    required this.correct_answer,
+    required this.incorrect_answer,
+  });
+
+  SportApi copyWith({
+    String? question,
+    String? correct_answer,
+    List<String>? incorrect_answer,
+  }) {
+    return SportApi(
+      question: question ?? this.question,
+      correct_answer: correct_answer ?? this.correct_answer,
+      incorrect_answer: incorrect_answer ?? this.incorrect_answer,
+    );
+  }
+
+  factory SportApi.fromMap(Map<String, dynamic> map) {
+    return SportApi(
+        question: map['question'] as String,
+        correct_answer: map['correct_answer'] as String,
+        incorrect_answer: List<String>.from(
+          (map['incorrect_answer'] as List<String>),
+        ));
+  }
+}
