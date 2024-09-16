dd3228686cc37944dfdd789950bdbc26f15bf2ec
Nadia Islam <sohasara853@gmail.com>
2024-09-16 11:46:18 +0600

    add quiz provider

--- lib/state/quiz_state.dart
+++ lib/state/quiz_state.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quiz_app/api/response.dart';
+import 'package:quiz_app/data/quiz_response.dart';
 
 final quizApiProvider = Provider((ref) => QuizApi());
+final quizProvider = FutureProvider<QuizResponse>((ref) async {
+  return ref.watch(quizApiProvider).fetchApi();
+});
