f49ad2e4acefa331b174b301711da9d41467cbba
Nadia Islam <sohasara853@gmail.com>
2024-09-17 11:17:34 +0600

    add return fuction

--- lib/ui/details/details_page.dart
+++ lib/ui/details/details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quiz_app/state/quiz_state.dart';
+import 'package:quiz_app/ui/details/options.dart';
 
 class DetailsPage extends ConsumerWidget {
   const DetailsPage({super.key});
@@ -16,7 +17,50 @@ class DetailsPage extends ConsumerWidget {
       body: quiz.when(data: (data) {
         return ListView.builder(
           itemCount: data.results.length,
-          itemBuilder: (context, index) {},
+          itemBuilder: (context, index) {
+            final question = data.results[index];
+            final ans = [...question.incorrectAnswer, question.correctAnswer];
+            return Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.purple[100]!,
+                    Colors.purple[200]!,
+                    Colors.purple[100]!
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomLeft,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    height: 200,
+                    width: 330,
+                    decoration: BoxDecoration(
+                      color: Colors.purple[300],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Text(
+                      '',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Options(),
+                ],
+              ),
+            );
+          },
         );
       }, error: (error, stackTrace) {
         return Center(
