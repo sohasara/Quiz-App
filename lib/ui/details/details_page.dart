a32016fe829ded9093b126d970f86c838411f9f1
Nadia Islam <sohasara853@gmail.com>
2024-09-17 11:12:38 +0600

    add list view

--- lib/ui/details/details_page.dart
+++ lib/ui/details/details_page.dart
@@ -1,59 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-//import 'package:quiz_app/state/quiz_state.dart';
-import 'package:quiz_app/ui/details/options.dart';
+import 'package:quiz_app/state/quiz_state.dart';
 
 class DetailsPage extends ConsumerWidget {
   const DetailsPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    //final quiz = ref.watch(quizProvider);
+    final quiz = ref.watch(quizProvider);
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple[100],
       ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.purple[100]!,
-              Colors.purple[200]!,
-              Colors.purple[100]!
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomLeft,
-          ),
-        ),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(15),
-              height: 200,
-              width: 330,
-              decoration: BoxDecoration(
-                color: Colors.purple[300],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Text(
-                '',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Options(),
-          ],
-        ),
-      ),
+      body: quiz.when(data: (data) {
+        return ListView.builder(
+          itemCount: data.results.length,
+          itemBuilder: (context, index) {},
+        );
+      }, error: (error, stackTrace) {
+        return Center(
+          child: Text(error.toString()),
+        );
+      }, loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }),
     );
   }
 }
