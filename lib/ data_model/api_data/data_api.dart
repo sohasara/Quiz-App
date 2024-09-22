31d69904890ecba7c31319182d1c1828b264854d
Nadia Islam <sohasara853@gmail.com>
2024-09-22 19:32:54 +0600

    add api data

---
+++ lib/ data_model/api_data/data_api.dart
@@ -0,0 +1,67 @@
+final apiData = [
+  [
+    {
+      "category": "Science &amp; Nature",
+      "question":
+          "When the Falcon Heavy was launched on it&#039;s test flight, what was the only part of the operation that failed?",
+      "correct_answer": "Center Core Landing",
+      "incorrect_answers": [
+        "Side Booster Landing",
+        "Deployment of Starman",
+        "Ignition and Liftoff"
+      ]
+    },
+    {
+      "category": "Science &amp; Nature",
+      "question": "Which of these is a type of stretch/deep tendon reflex?",
+      "correct_answer": "Ankle jerk reflex",
+      "incorrect_answers": [
+        "Gag reflex",
+        "Pupillary light reflex",
+        "Scratch reflex"
+      ]
+    },
+    {
+      "category": "Science &amp; Nature",
+      "question": "Which of the following spacecraft never touched the moon?",
+      "correct_answer": "Mariner 4",
+      "incorrect_answers": ["Apollo 11", "Luna 2", "SMART-1"]
+    },
+    {
+      "category": "Science &amp; Nature",
+      "question": "What is the half-life of Uranium-235?",
+      "correct_answer": "703,800,000 years",
+      "incorrect_answers": [
+        "4,300,400,000 years",
+        "1,260,900,000 years",
+        "Uranium-235 is a stable isotope"
+      ]
+    },
+  ],
+  [
+    {
+      "category": "General Knowledge",
+      "question": "What direction does the Statue of Liberty face?",
+      "correct_answer": "Southeast",
+      "incorrect_answers": ["Southwest", "Northwest", "Northeast"]
+    },
+    {
+      "category": "General Knowledge",
+      "question":
+          "Which logical fallacy means to attack the character of your opponent rather than their arguments?",
+      "correct_answer": "Ad hominem",
+      "incorrect_answers": [
+        "Post hoc ergo propter hoc",
+        "Tu quoque",
+        "Argumentum ad populum"
+      ]
+    },
+    {
+      "category": "General Knowledge",
+      "question":
+          "What character was once considered to be the 27th letter of the alphabet?",
+      "correct_answer": "Ampersand",
+      "incorrect_answers": ["Interrobang", "Tilde", "Pilcrow"]
+    },
+  ],
+];
