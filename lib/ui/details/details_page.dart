e91fefddd83ba6bb9da954dadb5e58a921485cf0
Nadia Islam <sohasara853@gmail.com>
2024-09-22 10:22:57 +0600

    modify the styling

--- lib/ui/details/details_page.dart
+++ lib/ui/details/details_page.dart
@@ -17,6 +17,7 @@ class DetailsPage extends ConsumerWidget {
         ref.watch(selectedAnswerProvider); // Track selected answer
 
     return Scaffold(
+      backgroundColor: Colors.purple[100],
       appBar: AppBar(
         backgroundColor: Colors.purple[100],
       ),
@@ -30,138 +31,154 @@ class DetailsPage extends ConsumerWidget {
           // Combine correct and incorrect answers into one list
           List<String> ans = [...incorrectAnswers, correctAnswer];
 
-          return Column(
-            children: [
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.purple[100]!,
-                        Colors.purple[200]!,
-                        Colors.purple[100]!
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomLeft,
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.purple[100]!,
+                          Colors.purple[200]!,
+                          Colors.purple[100]!
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomLeft,
+                      ),
                     ),
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(15),
-                        height: 200,
-                        width: 385,
-                        decoration: BoxDecoration(
-                          color: Colors.purple[300],
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: SingleChildScrollView(
-                          child: Text(
-                            questionText,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(15),
+                          height: 200,
+                          width: 385,
+                          decoration: BoxDecoration(
+                            color: Colors.purple[300],
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: SingleChildScrollView(
+                            child: Text(
+                              questionText,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      // Display options
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: ans.length,
-                          itemBuilder: (context, index) {
-                            final option = ans[index];
-                            final isCorrect = option == correctAnswer;
-                            final isSelected = option == selectedAnswer;
+                        const SizedBox(height: 20),
+                        // Display options
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: ans.length,
+                            itemBuilder: (context, index) {
+                              final option = ans[index];
+                              final isCorrect = option == correctAnswer;
+                              final isSelected = option == selectedAnswer;
 
-                            return GestureDetector(
-                              onTap: () {
-                                ref
-                                    .read(selectedAnswerProvider.notifier)
-                                    .state = option; // Set the selected answer
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 8.0,
-                                  horizontal: 15.0,
-                                ),
-                                child: Container(
-                                  padding: const EdgeInsets.all(15),
-                                  decoration: BoxDecoration(
-                                    color: Colors.purple[300],
-                                    borderRadius: BorderRadius.circular(20),
-                                    border: Border.all(
-                                      color: isSelected
-                                          ? (isCorrect
-                                              ? Colors.green
-                                              : Colors.red)
-                                          : Colors
-                                              .transparent, // Green if correct, Red if wrong
-                                      width: 4.0,
-                                    ),
+                              return GestureDetector(
+                                onTap: () {
+                                  ref
+                                          .read(selectedAnswerProvider.notifier)
+                                          .state =
+                                      option; // Set the selected answer
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0,
+                                    horizontal: 15.0,
                                   ),
-                                  child: Center(
-                                    child: Text(
-                                      option,
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        color: Colors.white,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(15),
+                                    decoration: BoxDecoration(
+                                      color: Colors.purple[300],
+                                      borderRadius: BorderRadius.circular(20),
+                                      border: Border.all(
+                                        color: isSelected
+                                            ? (isCorrect
+                                                ? Colors.green
+                                                : Colors.red)
+                                            : Colors
+                                                .transparent, // Green if correct, Red if wrong
+                                        width: 4.0,
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        option,
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.white,
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              // The "Next Question" button
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (currentQuestionIndex < data.results.length - 1) {
-                      // Reset selected answer when moving to the next question
-                      ref.read(currentQuestionIndexProvider.notifier).state++;
-                      ref.read(selectedAnswerProvider.notifier).state = null;
-                    } else {
-                      // If it's the last question, show a completion dialog or reset the quiz
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('Quiz Completed'),
-                          content: const Text(
-                              'You have answered all the questions!'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                // Reset the quiz or navigate to another page
-                                Navigator.of(context).pop();
-                                ref
-                                    .read(currentQuestionIndexProvider.notifier)
-                                    .state = 0;
-                                ref
-                                    .read(selectedAnswerProvider.notifier)
-                                    .state = null;
-                              },
-                              child: const Text('Restart'),
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text('Next Question'),
+                // The "Next Question" button
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple[300],
+                      padding: const EdgeInsets.all(20),
+                    ),
+                    onPressed: () {
+                      if (currentQuestionIndex < data.results.length - 1) {
+                        // Reset selected answer when moving to the next question
+                        ref.read(currentQuestionIndexProvider.notifier).state++;
+                        ref.read(selectedAnswerProvider.notifier).state = null;
+                      } else {
+                        // If it's the last question, show a completion dialog or reset the quiz
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Quiz Completed'),
+                            content: const Text(
+                                'You have answered all the questions!'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  // Reset the quiz or navigate to another page
+                                  Navigator.of(context).pop();
+                                  ref
+                                      .read(
+                                          currentQuestionIndexProvider.notifier)
+                                      .state = 0;
+                                  ref
+                                      .read(selectedAnswerProvider.notifier)
+                                      .state = null;
+                                },
+                                child: const Text('Restart'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text(
+                      'Next Question',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           );
         },
         error: (error, stackTrace) {
