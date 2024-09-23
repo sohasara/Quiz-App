c98ec3914b013fc5f3217ed29f997d091f0ed28f
Nadia Islam <sohasara853@gmail.com>
2024-09-23 15:05:18 +0600

    add question model

---
+++ lib/ data_model/question_model.dart
@@ -0,0 +1,23 @@
+class Question {
+  final String question;
+  final List<String> options;
+  final String correctAnswer;
+
+  Question({
+    required this.question,
+    required this.options,
+    required this.correctAnswer,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    List<String> options = List<String>.from(json['incorrect_answers']);
+    options.add(json['correct_answer']);
+    options.shuffle(); // Shuffle so the correct answer isn't always last
+
+    return Question(
+      question: json['question'],
+      options: options,
+      correctAnswer: json['correct_answer'],
+    );
+  }
+}
