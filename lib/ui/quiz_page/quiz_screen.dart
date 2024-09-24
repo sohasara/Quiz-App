3e40a1386ecc92d843f3c9a80b263a170d8e92af
Nadia Islam <sohasara853@gmail.com>
2024-09-24 10:58:09 +0600

    do app bar's styling

--- lib/ui/quiz_page/quiz_screen.dart
+++ lib/ui/quiz_page/quiz_screen.dart
@@ -15,12 +15,26 @@ class QuizScreen extends ConsumerWidget {
 
     return Scaffold(
       backgroundColor: Colors.purple[200],
-      appBar: AppBar(title: Text('Quiz: ${category.name}')),
+      appBar: AppBar(
+        backgroundColor: Colors.purple[200],
+        centerTitle: true,
+        title: Text(
+          'Quiz: ${category.name}',
+          style: const TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            // color: Colors.white,
+          ),
+        ),
+      ),
       body: quizQuestions.when(
         data: (questions) => DetailsPage(questions: questions),
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) =>
-            Center(child: Text('Failed to load questions: $error')),
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        error: (error, stack) => Center(
+          child: Text('Failed to load questions: $error'),
+        ),
       ),
     );
   }
