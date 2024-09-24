43aa438be4844d3f8a072d066add5e2f1bfe04ce
Nadia Islam <sohasara853@gmail.com>
2024-09-24 09:35:44 +0600

    add data

--- lib/ data_model/category_model.dart
+++ lib/ data_model/category_model.dart
@@ -8,3 +8,42 @@ class CategoryModel {
     required this.apiurl,
   });
 }
+
+final List<CategoryModel> categories = [
+  CategoryModel(
+    name: 'Sports',
+    imageurl: 'assets/football.png',
+    apiurl:
+        'https://opentdb.com/api.php?amount=10&category=21&difficulty=medium',
+  ),
+  CategoryModel(
+    name: 'Math',
+    imageurl: 'assets/math.png',
+    apiurl:
+        'https://opentdb.com/api.php?amount=10&category=19&difficulty=medium',
+  ),
+  CategoryModel(
+    name: 'Flims',
+    imageurl: 'assets/film.png',
+    apiurl:
+        'https://opentdb.com/api.php?amount=10&category=11&difficulty=hard&type=multiple',
+  ),
+  CategoryModel(
+    name: 'Geography',
+    imageurl: 'assets/geo.png',
+    apiurl:
+        'https://opentdb.com/api.php?amount=10&category=22&difficulty=medium',
+  ),
+  CategoryModel(
+    name: 'GK',
+    imageurl: 'assets/gk.png',
+    apiurl:
+        'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple',
+  ),
+  CategoryModel(
+    name: 'Politic',
+    imageurl: 'assets/politic.png',
+    apiurl:
+        'https://opentdb.com/api.php?amount=10&category=24&difficulty=medium&type=multiple',
+  ),
+];
