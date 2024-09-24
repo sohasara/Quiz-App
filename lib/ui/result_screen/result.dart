34201e9077cd90fe7edcf0fc5b76add934a71816
Nadia Islam <sohasara853@gmail.com>
2024-09-24 19:31:43 +0600

    modify

--- lib/ui/result_screen/result.dart
+++ lib/ui/result_screen/result.dart
@@ -55,7 +55,7 @@ class ResultScreen extends StatelessWidget {
                 children: [
                   Text(
                     correctAnswers.toString(),
-                  )
+                  ),
                 ],
               ),
             ),
