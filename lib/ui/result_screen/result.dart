00b3b5b521d394fbec16ca75d9c2bccc16e51957
Nadia Islam <sohasara853@gmail.com>
2024-09-24 15:19:55 +0600

    remove center

--- lib/ui/result_screen/result.dart
+++ lib/ui/result_screen/result.dart
@@ -25,21 +25,19 @@ class ResultScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'You answered $correctAnswers out of $totalQuestions correctly!',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context); // Go back to category screen
-              },
-              child: const Text('Try Again'),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'You answered $correctAnswers out of $totalQuestions correctly!',
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context); // Go back to category screen
+            },
+            child: const Text('Try Again'),
+          ),
+        ],
       ),
     );
   }
