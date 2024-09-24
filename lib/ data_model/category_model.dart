fafe983883e6b06acdc64570074774540107d3ed
Nadia Islam <sohasara853@gmail.com>
2024-09-24 09:28:06 +0600

    add category model

---
+++ lib/ data_model/category_model.dart
@@ -0,0 +1,10 @@
+class CategoryModel {
+  final String name;
+  final String imageurl;
+  final String apiurl;
+  CategoryModel({
+    required this.name,
+    required this.imageurl,
+    required this.apiurl,
+  });
+}
